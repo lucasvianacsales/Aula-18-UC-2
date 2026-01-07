c13307d45bf92fa21706a997999d95323b0ba842
Lucas Sales <100038556+lucasvianacsales@users.noreply.github.com>
2026-01-07 19:57:46 -0300

    Update analise_loja.py

--- analise_loja.py
+++ analise_loja.py
@@ -1,6 +1,7 @@
 import base_loja
 
 maior_valor = 0
+menor_valor = 999999
 total_vendas = 0
 
 for venda in base_loja.vendas["vendas"]:
@@ -8,9 +9,14 @@ for venda in base_loja.vendas["vendas"]:
 
     if valor_total > maior_valor:
         maior_valor = valor_total
-
     
-        
-print("Maior venda")
-print(f"Valor total: R$ {maior_valor:.2f}")
+    if valor_total < menor_valor:
+        menor_valor = valor_total
 
+
+    total_vendas += valor_total
+        
+
+print(f"Maior venda: R$ {maior_valor:.2f}")
+print(f"Menor venda: R$ {menor_valor:.2f}")
+print(f"Total de vendas: R$ {total_vendas:.2f}")
